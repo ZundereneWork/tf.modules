c0de2461e28a5b58fbaa6e9b53b8d44617e30901
hector21450 <hector21450@gmail.com>
2023-03-19 12:47:45 +0100

    add more congif at aks

--- Aks/main.tf
+++ Aks/main.tf
@@ -10,18 +10,38 @@ resource "azurerm_kubernetes_cluster" "aks" {
   pod_cidr            = var.pod_cidr
 
   default_node_pool {
-    name            = "${var.aks_name}-default-np"
-    node_count      = var.numNodes
-    vm_size         = var.size
-    type            = var.type
-    vnet_subnet_id  = var.subnet_id
-    max_pods        = var.maxNode
-    os_disk_size_gb = var.disk_size_gb
+    name                    = "${var.aks_name}-default-np"
+    vm_size                 = var.size
+    node_count              = var.numNodes
+    type                    = var.type
+    vnet_subnet_id          = var.subnet_id
+    max_pods                = var.maxNode
+    os_disk_size_gb         = var.disk_size_gb
+    vnet_subnet_id          = var.aget_pool_subnet_id
+    enabled_node_public_ip  = false
+    orchestrator_version    = var.kubernetes_version
+ }
+
+ network_profile {
+  network_plugin      = var.network_plugin
+  load_balace_sku     = var.load_balance_sku
+  outbound_type       = var.outbound_type
+  network_policy      = var.network_policy
+  dns_service_ip      = var.dns_service_ip
+  docker_bridge_cidr  = var.docker_bridge_cidr
+  service_cidr        = var.service_cidr
+  pod_cidr            = var.network_policy == 'kubenet' ? var.ip_range_pod : null
+
  }
 
  service_principal {
     client_id     = var.client_id
     client_secret = var.client_secret
+  }
+
+  azure_active_directory_role_based_access_control {
+    managed = true
+    admin_group_object_ids = concat([], var.list_add_group_ids)
   }
 
   addon_profile {
@@ -31,8 +51,9 @@ resource "azurerm_kubernetes_cluster" "aks" {
   }
 }
 
+
 resource "azurerm_role_assignment" "aks_acrpull" {
-  scope              = var.azurerm_container_registry_id"
-  role_definition_id = "/subscriptions/<subscription_id>/providers/Microsoft.Authorization/roleDefinitions/69e1f4b9-eb29-42b2-afd4-4b0c986de60c" //ID del role AcrPull
+  scope              = var.azurerm_container_registry_id
+  role_definition_id = "/subscriptions/910c5d1c-f32e-48a9-8068-a94718a51840/providers/Microsoft.Authorization/roleDefinitions/69e1f4b9-eb29-42b2-afd4-4b0c986de60c" //ID del role AcrPull
   principal_id       = "${azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id}"
 }
--- Aks/variables.tf
+++ Aks/variables.tf
@@ -1,68 +1,123 @@
 variable "name" {
-  type = string
+  type        = string
+  description = "The name of the AKS cluster."
 }
 
 variable "loc" {
-  type = string
+  type        = string
+  description = "The location where the AKS cluster will be created."
 }
 
 variable "count" {
-  type = number
+  type        = string
+  description = "A count used to generate unique resource names."
 }
 
-variable "version_aks" {
-  type = string
+variable "location" {
+  type        = string
+  description = "The location where the resource group will be created."
 }
 
 variable "resource_group_name" {
-  type = string
+  type        = string
+  description = "The name of the resource group that the AKS cluster will be created in."
+}
+
+variable "version_aks" {
+  type        = string
+  description = "The version of Kubernetes to use for the AKS cluster."
 }
 
 variable "service_cidr" {
-  type = string
+  type        = string
+  description = "The CIDR block used for Kubernetes services."
 }
 
 variable "dns_service_ip" {
-  type = string
+  type        = string
+  description = "The IP address used for Kubernetes DNS services."
 }
 
 variable "pod_cidr" {
-  type = string
-}
-
-variable "numNodes" {
-  type = number
+  type        = string
+  description = "The CIDR block used for Kubernetes pods."
 }
 
 variable "size" {
-  type = string
+  type        = string
+  description = "The size of the virtual machines used in the default node pool."
+}
+
+variable "numNodes" {
+  type        = number
+  description = "The number of nodes in the default node pool."
 }
 
 variable "type" {
-  type = string
+  type        = string
+  description = "The type of virtual machine used in the default node pool."
 }
 
 variable "subnet_id" {
-  type = string
+  type        = string
+  description = "The ID of the subnet where the default node pool will be created."
 }
 
 variable "maxNode" {
-  type = number
+  type        = number
+  description = "The maximum number of pods that can run on a node."
 }
 
 variable "disk_size_gb" {
-  type = number
+  type        = number
+  description = "The size of the OS disk in the default node pool."
+}
+
+variable "aget_pool_subnet_id" {
+  type        = string
+  description = "The ID of the subnet where the agent pool will be created."
+}
+
+variable "network_plugin" {
+  type        = string
+  description = "The network plugin used for the AKS cluster."
+}
+
+variable "load_balance_sku" {
+  type        = string
+  description = "The SKU of the load balancer used for the AKS cluster."
+}
+
+variable "outbound_type" {
+  type        = string
+  description = "The outbound type used for the AKS cluster."
+}
+
+variable "network_policy" {
+  type        = string
+  description = "The network policy used for the AKS cluster."
+}
+
+variable "ip_range_pod" {
+  type        = string
+  description = "The IP range used for Kubernetes pods when the network policy is 'kubenet'."
 }
 
 variable "client_id" {
-  type = string
+  type        = string
+  description = "The client ID of the service principal used to manage the AKS cluster."
 }
 
 variable "client_secret" {
-  type = string
+  type        = string
+  description = "The client secret of the service principal used to manage the AKS cluster."
 }
 
 variable "azurerm_container_registry_id" {
-  type = string
+  type        = string
+  description = "The ID of the Azure Container Registry to grant pull access to the AKS cluster."
 }
 
+variable "list_add_group_ids" {
+  type        = list(string)
+  description = "A list of Azure AD group IDs to add as admins for the AK
